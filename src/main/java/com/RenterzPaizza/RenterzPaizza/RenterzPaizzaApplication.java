6ec6ad886d49f4cac01a9a5515bfcdadb44ada2d
SEENIVASAN-VENKATESAN <seenivasan.tech@gmail.com>
2026-01-30 15:36:54 +0530

    Projects First Commit

---
+++ src/main/java/com/RenterzPaizza/RenterzPaizza/RenterzPaizzaApplication.java
@@ -0,0 +1,13 @@
+package com.RenterzPaizza.RenterzPaizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenterzPaizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenterzPaizzaApplication.class, args);
+	}
+
+}
